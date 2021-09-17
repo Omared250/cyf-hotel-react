b115995170799f5ae45b0b2551e2e6a8fe2f503e
Omar <omared250@gmail.com>
2021-09-18 01:21:18 +0200

    exercise 12 completed

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import React from "react";
+import Order from "./Order";
 
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-
-  const orderOne = () => {
-    setOrders(orders + 1);
-  };
-
   return (
     <div className="container d-flex flex-column align-items-center justify-content-center">
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders} <RestaurantButton handler={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
