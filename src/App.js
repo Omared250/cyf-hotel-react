ea4f75ac50889faf8b5806f8de7071b3e8424531
Omar <omared250@gmail.com>
2021-09-30 21:09:05 +0200

    working on extra exercise

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Bookings from "./components/Bookings";
 import "./App.css";
 import Restaurant from "./components/Restaurant";
+import NewBooking from "./components/NewBooking";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Bookings />
       <Restaurant />
       <TouristInfoCard />
+      <NewBooking />
       <Footer />
     </div>
   );
